f58e37fb8e4653828c670679221a9cce2bccac52
Duy La (Jayce) <36068829+lpduy69@users.noreply.github.com>
2022-10-18 13:16:13 +0700

    Create clear_MSTeams_cache.ps1

---
+++ clear_MSTeams_cache.ps1
@@ -0,0 +1,9 @@
+Remove-Item –path "C:\Users\*\AppData\Roaming\\Microsoft\teams\application cache\cache\*”
+Remove-Item –path "C:\Users\*\AppData\Roaming\\Microsoft\teams\blob_storage\*”
+Remove-Item –path "C:\Users\*\AppData\Roaming\\Microsoft\teams\databases\*”
+Remove-Item –path "C:\Users\*\AppData\Roaming\\Microsoft\teams\GPUcache\*”
+Remove-Item –path "C:\Users\*\AppData\Roaming\\Microsoft\teams\IndexedDB\*” -recurse
+Remove-Item –path "C:\Users\*\AppData\Roaming\\Microsoft\teams\Local Storage\*” -recurse
+Remove-Item –path "C:\Users\*\AppData\Roaming\\Microsoft\teams\tmp\*”
+Remove-Item –path "C:\Users\*\AppData\Roaming\\Microsoft\teams\Cache\*”
+Remove-Item –path "C:\Users\*\AppData\Roaming\Microsoft\teams\backgrounds\*”
